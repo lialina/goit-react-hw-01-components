080b84121491d2e7934c3a51baee271f5030ffbd
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2021-07-31 23:17:02 +0300

    Update components

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -5,19 +5,26 @@ function TransactionHistory({ items }) {
   return (
     <table className={s.table}>
       <thead className={s.head}>
-        <tr className={s.header}>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+        <tr>
+          <th className={s.header}>Type</th>
+          <th className={s.header}>Amount</th>
+          <th className={s.header}>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {items.map(item => (
-          <tr key={item.id} className={s.row}>
-            <td>{item.type}</td>
-            <td>{item.amount}</td>
-            <td>{item.currency}</td>
+          <tr
+            key={item.id}
+            className={s.row}
+            style={{
+              backgroundColor:
+                items.indexOf(item) % 2 === 0 ? 'white' : 'rgb(228, 228, 228)',
+            }}
+          >
+            <td className={s.type}>{item.type}</td>
+            <td className={s.amount}>{item.amount}</td>
+            <td className={s.currency}>{item.currency}</td>
           </tr>
         ))}
       </tbody>
